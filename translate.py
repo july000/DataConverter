03d0a413cc7bd651a8578e0abf16341aa3baafa6
renjunmei <renjunmei@51hitech.com>
2023-06-27 16:30:37 +0800

    Add command line arguments.

--- translate.py
+++ translate.py
@@ -4,6 +4,7 @@ import sys
 import math
 import pymap3d
 import json
+import argparse
 
 def load_json(input_file):
     with open(input_file) as f:
@@ -14,7 +15,7 @@ def read_config_file(config_file):
     # case-sensitive when read from config file
     config.optionxform = str
     try:
-        config.read(config_file)
+        config.read(config_file, encoding='utf-8')
         file_type = config['FileType']
         json_param = config['Json']
         constant = config.items('Const')
@@ -47,22 +48,22 @@ def process_data_frame(df, constant, column_settings, series_conversion_rules, d
     # 根据指定的列名，选择 DataFrame 的列
     src_column_names = list(set(name for name in list(column_settings.values()) if name != 'None'))
     df = df[src_column_names]
-    generated_df = pd.DataFrame(columns=list(column_settings.keys()))
+    dst_df = pd.DataFrame(columns=list(column_settings.keys()))
 
     # 设计到一列的转换规则
     for dst_column_name, series_rule in series_conversion_rules.items():
         dst_column_name = dst_column_name.strip()
         src_column_name = column_settings[dst_column_name]
-        generated_df[dst_column_name] = df[src_column_name].apply(eval(series_rule))
+        dst_df[dst_column_name] = df[src_column_name].apply(eval(series_rule))
     
     # 涉及到多列的使用
     for dst_column_name, df_rule in dataframe_conversion_rules.items():
         dst_column_name = dst_column_name.strip().split(', ')
         if len(dst_column_name) <= 1:
             dst_column_name = dst_column_name[0]
-            generated_df[dst_column_name] = df.apply((eval(df_rule)), axis=1)
+            dst_df[dst_column_name] = df.apply((eval(df_rule)), axis=1)
         else:
-            generated_df[dst_column_name] = pd.DataFrame(list(df.apply((eval(df_rule.replace('${Const:', '{').format(**constant))), axis=1)), columns=dst_column_name)
+            dst_df[dst_column_name] = pd.DataFrame(list(df.apply((eval(df_rule.replace('${Const:', '{').format(**constant))), axis=1)), columns=dst_column_name)
     
     # generated_df = generated_df.assign(**{col: df.apply(eval(rule), axis=1) for col, rule in dataframe_conversion_rules.items()})
     
@@ -70,7 +71,7 @@ def process_data_frame(df, constant, column_settings, series_conversion_rules, d
     # for const_column_name, value in constant.items():
     #     if const_column_name in list(column_settings.keys()):
     #         generated_df[const_column_name] = eval(value)
-    return generated_df
+    return dst_df
 
 # 生成 CSV 文件，输出转换后的数据
 def write_csv_file(output_file, df):
@@ -82,16 +83,13 @@ def write_csv_file(output_file, df):
         sys.exit()
 
 if __name__ == '__main__':
-    config_file = 'config.ini'
-    file_type, json_param, constant, column_settings, series_conversion_rules, dataframe_conversion_rules = read_config_file(config_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config-file', dest='config_file', default='F:\\renjunmei007\\05_code\\github\\DataConverter\\config.ini')
+    parser.add_argument('--input-file', dest='input_file', default='F:\\renjunmei007\\05_code\\github\\DataConverter\\sample_case\\offset_flatten_rsm_30_all.csv')
+    parser.add_argument('--output-file', dest='output_file', default='F:\\renjunmei007\\05_code\\github\\DataConverter\\output.csv')
+    args = parser.parse_args()
 
-    input_file = 'offset_flatten_rsm_30_all.csv'
-    # input_file = 'G:\\CyTrafficEditor\\CyTraffic\\data\\test_cases\\zhilian\\dumps\\1687321540.json'
-    df = read_file(file_type, json_param, input_file)
-
-    # 根据列名和转换规则进行 DataFrame 转换
-    generated_df = process_data_frame(df, constant, column_settings, series_conversion_rules, dataframe_conversion_rules)
-
-    # 生成转换后的 CSV 文件
-    output_file = 'output.csv'
-    write_csv_file(output_file, generated_df)
+    file_type, json_param, constant, column_settings, series_conversion_rules, dataframe_conversion_rules = read_config_file(args.config_file)
+    src_df = read_file(file_type, json_param, args.input_file)
+    dst_df = process_data_frame(src_df, constant, column_settings, series_conversion_rules, dataframe_conversion_rules)
+    write_csv_file(args.output_file, dst_df)
